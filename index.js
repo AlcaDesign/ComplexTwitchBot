318998606be05d64ec549e01ad4c599ecfff4d55
AlcaDesign <jfost3@gmail.com>
2017-02-04 04:42:27 -0600

    Update

--- index.js
+++ index.js
@@ -32,9 +32,10 @@ let client = new tmi.client({
 		// The "identity" of the bot. Username and OAuth token from the config.
 		identity: config.twitch.bot.identity,
 		// The channels to automatically join upon connecting.
-		channels: config.twitch.bot.channels
+		channels: config.twitch.bot.channels.slice()
 	});
 
+// Load the commands.
 commands.load();
 
 // Attach the listener and some basic setup.
@@ -49,19 +50,31 @@ client.on('message', function(_channel, _userstate, message, self) {
 	let userstate = utils.kebabToSnake(_userstate),
 		// Remove the "#" from the channel name. This might change, so it's pre-
 		// pared for if the "#" is no longer passed with it from the library.
-		chan = _channel[0] === '#' ? _channel.slice(1) : channel;
+		chan = _channel[0] === '#' ? _channel.slice(1) : _channel;
 	
 	// "userstate.name" is good for when you're actually talking in the chat. It
 	// will use the display name of the user if available and fallback to the
 	// lowercase "login" username from the userstate.
 	userstate.name = userstate.display_name || userstate.username;
+	userstate._id = userstate.user_id;
 	
 	// Skip the first character and split the message by spaces.
 	let params = message.slice(1).split(' '),
 		// Just the command name out of the params, lowercased.
 		commandName = params.shift().toLowerCase(),
-		// The permissions for the user.
-		perms = commands.getPermissions(chan, userstate),
+		// Find the command from the core list.
+		command = commands.find(commandName);
+	
+	// No command found. You may wish to use custom channel commands and this is
+	// the point where you should check wherever for this channel's custom
+	// commands. This function may need to be changed slightly to accommodate
+	// for the async calls to the database.
+	if(command === null) {
+		return;
+	}
+	
+	// The permissions for the user.
+	let perms = commands.getPermissions(chan, userstate),
 		// A basic channel object for use in commands.
 		channel = {
 			// The Twitch ID of the channel.
@@ -72,6 +85,9 @@ client.on('message', function(_channel, _userstate, message, self) {
 			// then use the display name from the userstate.
 			name: perms.broadcaster ? userstate.name : chan
 		},
+		// Get the channel object from the API when the bot loaded.
+		channelAPI = config.twitch.bot.channels
+			.find(n => n.name === chan) || null,
 	
 		// Major context from the message.
 		messageContext = {
@@ -95,36 +111,121 @@ client.on('message', function(_channel, _userstate, message, self) {
 			chan, userstate, message, self,
 			params, commandName, perms, channel,
 			actions
-		},
-		// Find the command from the core list.
-		command = commands.find(commandName);
+		};
+
+	// If the API data is available for this channel, add it to the channel
+	// object for use in commands.
+	if(channelAPI !== null) {
+		Object.assign(channel, channelAPI);
+	}
 	
-	// No commands found.
-	if(command === null) {
+	// Check if there are parameters to use.
+	params.has = params.length > 0;
+	// Use which channel permissions?
+	let comPerm = true,
+		// Use which channel permissions for params?
+		comPP = false;
+	// Check for the partner permissions.
+	if(channel.partner && 'partner' in command.permissions) {
+		comPerm = command.permissions.partner;
+	}
+	else {
+		// Check for the non-partner permissions.
+		comPerm = 'notPartner' in command.permissions ?
+			command.permissions.notPartner : comPerm;
+	}
+	// If comPerm is null, it's the default "true."
+	if(comPerm === null) {
+		comPerm = true;
+	}
+	
+	// Check for the partner permissions.
+	if(channel.partner && 'useParamsPartner' in command.permissions) {
+		comPP = command.permissions.useParamsPartner;
+	}
+	else {
+		// Check for the non-partner permissions.
+		comPP = 'useParams' in command.permissions ?
+			command.permissions.useParams : comPP;
+	}
+	
+	// Check if there are parameters to use and the user has permission to use
+	// them for this command.
+	params.use = params.has &&
+		(
+			// Check that "useParams" is a key in the permissions for the
+			// command.
+			'useParams' in command.permissions &&
+			(
+				// The params could be "true" or "null" meaning true.
+				[true, null].indexOf(comPP) > -1 ||
+				// Or, check them against the permissions object.
+				perms[comPP]
+			)
+		);
+	
+	// Check that the user has permission to use the command.
+	if(comPerm === false || (typeof comPerm === 'string' && !perms[comPerm])) {
+		return;
+	}
+	// Check that the user has the necessary parameters for the command.
+	else if(
+		(comPP === true && !params.has) ||
+		(typeof comPP === 'number' && comPP > 0 && params.length < comPP)
+	) {
 		return;
 	}
 	
+	
 	// Let's try it out! Bind the actions object and use the better arguments.
 	command.action.call(actions, data)
-		// Looks like everything probably went well.
-		.then(data => {
-			// Congratulations.
+	// Looks like everything probably went well.
+	.then(data => {
+		// Congratulations.
+		if(data !== undefined) {
 			console.log(data);
-		})
-		// Something probably didn't happen or some error occurred.
-		.catch(err => {
-			// There /is/ an error.
-			if(err) {
-				// Log the error, etc.
-				console.log(err);
-			}
-			// All clear. Something still could have gone wrong, but it wasn't
-			// a bad issue.
-			else {
-				// Handle this somehow.
-			}
-		});
+		}
+	})
+	// Something probably didn't happen or some error occurred.
+	.catch(err => {
+		// There /is/ an error.
+		if(err) {
+			// Log the error, etc.
+			console.log(err);
+		}
+		// All clear. Something still could have gone wrong, but it wasn't
+		// a bad issue.
+		else {
+			// Handle this somehow.
+		}
+	});
 });
 
+// Get data from the Twitch API about each channel.
+let proms = config.twitch.bot.channels.map((n, i, a) =>
+		new Promise((resolve, reject) => {
+			let p = () => tAPI.userByName(n)
+				.then(data => {
+					if(data === null) {
+						return;
+					}
+					return tAPI.channel(data._id)
+					.then(({ res, body }) =>
+						a[i] = res.statusCode === 200 ? body : {}
+					);
+				})
+				.then(resolve)
+				.catch(reject);
+			setTimeout(p, 80 * i);
+		})
+	);
+
+// Wait for all of the channel API requests.
+Promise.all(proms)
 // Connect the client to the servers.
-client.connect();
+.then(() => client.connect())
+.catch(err => {
+	if(err) {
+		console.log(err);
+	}
+});
